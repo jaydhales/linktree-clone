da0e8a50fa79f02128d6add0db019da819c865ae
James Osunkiyesi <jaydhales@gmail.com>
2022-10-22 20:01:09 +0100

    Added links data into an array of objects

---
+++ src/Components/Main.jsx
@@ -0,0 +1,37 @@
+const Main = () => {
+  const links = [
+    {
+      id: "btn__twitter",
+      text: "Twitter Link",
+      href: "https://twitter.com/jaydhales1",
+    },
+    {
+      id: "btn__zuri",
+      text: "Zuri Team",
+      href: "https://training.zuri.team/",
+    },
+    {
+      id: "books",
+      text: "Zuri Books",
+      href: "http://books.zuri.team",
+    },
+    {
+      id: "book__python",
+      text: "Python Books",
+      href: "http://books.zuri.team/python-for-beginners?ref_id=",
+    },
+    {
+      id: "pitch",
+      text: "Background Check for Coders",
+      href: "https://background.zuri.team",
+    },
+    {
+      id: "book__design",
+      text: "Design Books",
+      href: "https://books.zuri.team/design-rules",
+    },
+  ];
+  return <section></section>;
+};
+
+export default Main;
