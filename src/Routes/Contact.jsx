267aad4c492709cf9c4645d497091f9382a68c32
James Osunkiyesi <jaydhales@gmail.com>
2022-11-04 03:44:54 +0100

    feat: added form validations

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
 import "../styles/Contact.css";
 
 const Contact = ({ name }) => {
+  const [contactData, setContactData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const [hasUserAgreed, setHasUserAgreed] = useState(false);
+
+  const handleChange = (e, key) => {
+    setErrors({});
+    setContactData({ ...contactData, [key]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors(validate(contactData));
+  };
+
+  const validate = (values) => {
+    let errors = {};
+    for (const key in values) {
+      if (!values[key]) {
+        switch (key) {
+          case "message":
+            errors[key] = `Please enter a message`;
+            break;
+          default:
+            errors[key] = `Please enter your ${key}`;
+            break;
+        }
+      }
+
+      if (values.email && !/\S+@\S+\.\S+/.test(values.email)) {
+        errors.email = "Email address is invalid";
+      }
+    }
+    return errors;
+  };
+
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e)} noValidate>
       <h2>Contact Me</h2>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
 
@@ -13,8 +56,14 @@ const Contact = ({ name }) => {
             type="text"
             id="first_name"
             placeholder="Enter your first name"
-            className="input"
+            className={`input ${errors.firstName && "is-danger"}`}
+            value={contactData.firstName}
+            onChange={(e) => handleChange(e, "firstName")}
           />
+
+          {errors.firstName && (
+            <p className="help is-danger">{errors.firstName}</p>
+          )}
         </div>
 
         <div className="form-group">
@@ -23,8 +72,13 @@ const Contact = ({ name }) => {
             type="text"
             id="last_name"
             placeholder="Enter your last name"
-            className="input"
+            className={`input ${errors.lastName && "is-danger"}`}
+            value={contactData.lastName}
+            onChange={(e) => handleChange(e, "lastName")}
           />
+          {errors.lastName && (
+            <p className="help is-danger">{errors.lastName}</p>
+          )}
         </div>
       </div>
 
@@ -34,8 +88,11 @@ const Contact = ({ name }) => {
           type="email"
           id="email"
           placeholder="yourname@gmail.com"
-          className="input"
+          className={`input ${errors.email && "is-danger"}`}
+          value={contactData.email}
+          onChange={(e) => handleChange(e, "email")}
         />
+        {errors.email && <p className="help is-danger">{errors.email}</p>}
       </div>
 
       <div className="form-group">
@@ -44,12 +101,20 @@ const Contact = ({ name }) => {
           name="message"
           id="message"
           placeholder="Send me a message and I'll reply you as soon as possible..."
-          className="input"
+          className={`input ${errors.message && "is-danger"}`}
+          value={contactData.message}
+          onChange={(e) => handleChange(e, "message")}
         />
+        {errors.message && <p className="help is-danger">{errors.message}</p>}
       </div>
 
       <div className="form-check">
-        <input type="checkbox" name="agree" id="agree" />
+        <input
+          type="checkbox"
+          name="agree"
+          id="agree"
+          onChange={(e) => setHasUserAgreed(e.target.checked)}
+        />
         <p>You agree to providing your data to {name} who may contact you.</p>
       </div>
 
@@ -58,6 +123,7 @@ const Contact = ({ name }) => {
         value="Send message"
         id="btn_submit"
         className="input"
+        disabled={!hasUserAgreed}
       />
     </form>
   );
